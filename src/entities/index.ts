b4c6aaa1a823b541c79f88f233ae5bdfa352133b
Wix Remote Machine <support@wix.com>
2026-01-10 03:33:19 +0000

    Create Client Portal with Features

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -29,6 +29,89 @@ export interface BlogPosts {
 
 
 /**
+ * Collection ID: clientbookings
+ * Interface for ClientBookings
+ */
+export interface ClientBookings {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  clientName?: string;
+  /** @wixFieldType text */
+  serviceType?: string;
+  /** @wixFieldType date */
+  appointmentDate?: Date | string;
+  /** @wixFieldType time */
+  appointmentTime?: any;
+  /** @wixFieldType text */
+  status?: string;
+  /** @wixFieldType url */
+  videoCallLink?: string;
+  /** @wixFieldType text */
+  trainerNotes?: string;
+}
+
+
+/**
+ * Collection ID: clientmessages
+ * Interface for ClientMessages
+ */
+export interface ClientMessages {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  conversationId?: string;
+  /** @wixFieldType text */
+  senderIdentifier?: string;
+  /** @wixFieldType text */
+  recipientIdentifier?: string;
+  /** @wixFieldType text */
+  messageContent?: string;
+  /** @wixFieldType datetime */
+  sentAt?: Date | string;
+  /** @wixFieldType boolean */
+  isRead?: boolean;
+}
+
+
+/**
+ * Collection ID: clientprograms
+ * Interface for ClientPrograms
+ */
+export interface ClientPrograms {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  programTitle?: string;
+  /** @wixFieldType text */
+  sessionTitle?: string;
+  /** @wixFieldType text */
+  workoutDay?: string;
+  /** @wixFieldType text */
+  exerciseName?: string;
+  /** @wixFieldType number */
+  sets?: number;
+  /** @wixFieldType number */
+  reps?: number;
+  /** @wixFieldType text */
+  weightOrResistance?: string;
+  /** @wixFieldType text */
+  tempo?: string;
+  /** @wixFieldType number */
+  restTimeSeconds?: number;
+  /** @wixFieldType text */
+  exerciseNotes?: string;
+  /** @wixFieldType number */
+  exerciseOrder?: number;
+  /** @wixFieldType url */
+  exerciseVideoUrl?: string;
+}
+
+
+/**
  * Collection ID: clienttestimonials
  * Interface for ClientTestimonials
  */
@@ -48,6 +131,77 @@ export interface ClientTestimonials {
   keyAchievement?: string;
   /** @wixFieldType boolean */
   featuredOnHomepage?: boolean;
+}
+
+
+/**
+ * Collection ID: nutritionguidance
+ * Interface for NutritionGuidance
+ */
+export interface NutritionGuidance {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  guidanceTitle?: string;
+  /** @wixFieldType text */
+  overview?: string;
+  /** @wixFieldType text */
+  mealPlanDetails?: string;
+  /** @wixFieldType text */
+  dietaryNotes?: string;
+  /** @wixFieldType date */
+  dateIssued?: Date | string;
+  /** @wixFieldType url */
+  supportingDocument?: string;
+}
+
+
+/**
+ * Collection ID: privatevideolibrary
+ * Interface for PrivateVideoLibrary
+ */
+export interface PrivateVideoLibrary {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  videoTitle?: string;
+  /** @wixFieldType text */
+  description?: string;
+  /** @wixFieldType url */
+  videoUrl?: string;
+  /** @wixFieldType text */
+  category?: string;
+  /** @wixFieldType text */
+  accessTags?: string;
+}
+
+
+/**
+ * Collection ID: progresscheckins
+ * Interface for ProgressCheckins
+ */
+export interface ProgressCheckins {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType datetime */
+  checkinDate?: Date | string;
+  /** @wixFieldType text */
+  clientNotes?: string;
+  /** @wixFieldType number */
+  currentWeight?: number;
+  /** @wixFieldType image */
+  progressPhotoFront?: string;
+  /** @wixFieldType image */
+  progressPhotoSide?: string;
+  /** @wixFieldType image */
+  progressPhotoBack?: string;
+  /** @wixFieldType text */
+  bodyMeasurements?: string;
+  /** @wixFieldType number */
+  energyLevel?: number;
 }
 
 
