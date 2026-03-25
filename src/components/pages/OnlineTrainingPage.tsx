5ab126b83da35897fedc184c25a3fd3914c81fc3
Wix Remote Machine <support@wix.com>
2026-03-25 04:09:54 +0000

    Set all sections to white background on online training page

--- src/components/pages/OnlineTrainingPage.tsx
+++ src/components/pages/OnlineTrainingPage.tsx
@@ -14,9 +14,9 @@ export default function OnlineTrainingPage() {
   };
 
   return (
-    <div className="bg-soft-white">
+    <div className="bg-white">
       {/* Hero Section */}
-      <section className="py-24 px-8 lg:px-20 bg-warm-sand-beige">
+      <section className="py-24 px-8 lg:px-20 bg-white">
         <div className="max-w-[100rem] mx-auto">
           <div className="grid md:grid-cols-2 gap-12 items-center">
             <div>
