262b9f4fb014d94886fba5daf9ba7dabf28f5bba
Wix Remote Machine <support@wix.com>
2026-05-18 22:55:39 +0000

    Reduce background image size

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -595,7 +595,7 @@ export default function HomePage() {
           >
             <Image
               src="https://static.wixstatic.com/media/93e866_e211b0c1e3844ba8906a49102799244c~mv2.jpg"
-              className="w-full h-full object-cover object-center"
+              className="w-full h-full object-contain object-center"
               width={1920}
               height={1080}
               alt="Strength Training for Women 35+ - Hero Background"
